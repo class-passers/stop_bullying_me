50d37e9fed4b06f2e9d202d5b2a06256150fd446
Younggi <cozyncoze@gmail.com>
2018-05-12 19:35:59 -0400

    started to implement zombie object

--- src/main_frame.js
+++ src/main_frame.js
@@ -1,27 +1,16 @@
-let canvas = document.querySelector("#game_frame");
+var canvas = document.querySelector("#game_frame");
 canvas.width = 1000;
 canvas.height = 800;
 
-let context = canvas.getContext("2d");
+var context = canvas.getContext("2d");
 
-let world_map_img = new Image();
+var world_map_img = new Image();
 world_map_img.src = "img/world_map.png";
 
-let renderObject = function( pos_x, pos_y, width, height, image ){
-    this.x = pos_x;
-    this.y = pos_y;
-    this.width = width;
-    this.height = height;
-    this.image = image;
-    this.update = function()
-    {
-
-    };
-};
-
-let objects = [];
-objects.push( new renderObject( 0, 0, 0, 0, '' ) );
+var objects = [];
+objects.push( new zombieObject( 0, 0, 256, 256 ) );
 update();
+
 function update()
 {
     for( var i = 0; i < objects.length; i++ )
@@ -37,11 +26,14 @@ function render()
 {
     // no need to clear context
     context.drawImage( world_map_img, 0, 0 );
-
     // draw objects
     for( var i = 0; i < objects.length; i++ )
     {
-        //context.drawImage( objects[i].image, objects.x, objects.y );
+        var obj = objects[i];
+
+        context.drawImage( obj.image, obj.get_source_x(), obj.get_source_y(),
+            obj.sprite_width, obj.sprite_height,
+            obj.x, obj.y, obj.width, obj.height );
     }
 
 }
