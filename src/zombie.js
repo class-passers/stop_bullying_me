7dfc282abb69f6582cbac3411f28a77f13946b58
Younggi <cozyncoze@gmail.com>
2018-05-12 23:35:41 -0400

    added a missing file

---
+++ src/zombie.js
@@ -0,0 +1,29 @@
+
+var zombieImage = new Image();
+zombieImage.src = "img/zombie_walk.png";
+
+var zombieObject = function( pos_x, pos_y, width, height ){
+    this.x = pos_x;
+    this.y = pos_y;
+    this.width = width;
+    this.height = height;
+    this.sprite_width = zombieImage.width / 4;
+    this.sprite_height = zombieImage.height / 3;
+    this.max_num_sprites = 10;
+    this.image = zombieImage;
+    this.index = 0;
+    this.get_source_x = function()
+    {
+        return this.sprite_width * ( parseInt(this.index) % 4 )
+    };
+    this.get_source_y = function()
+    {
+        return this.sprite_height * parseInt( parseInt(this.index) / 4 );
+    };
+    this.update = function()
+    {
+        this.index += 0.2;
+        if( this.index >= this.max_num_sprites )
+            this.index = 0;
+    };
+};
