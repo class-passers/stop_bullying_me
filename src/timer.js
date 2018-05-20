e6ebeac2d7a5df2f1b482bdaa9e1029b4defab20
Maksim <34315466+whiteTrailer@users.noreply.github.com>
2018-05-20 13:40:33 -0400

    Create timer.js

---
+++ src/timer.js
@@ -0,0 +1,11 @@
+var timer = setInterval(countTimer, 1000);
+var totalSec = 0;
+function countTimer() 
+{
+   ++totalSec;
+   var hour = Math.floor(totalSec /3600);
+   var min = Math.floor((totalSec - hour*3600)/60);
+   var sec = totalSec - (hour*3600 + min*60);
+
+   document.getElementById(“game_frame”).innerHTML = hour + ":" + min + ":" + sec;
+}
