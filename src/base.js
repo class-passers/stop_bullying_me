ab024ac14b952977a2e6a592ce2d0a0c2a307e23
Jin <bomjan@naver.com>
2018-05-24 21:42:40 -0400

    Added comment

--- src/base.js
+++ src/base.js
@@ -1,11 +1,11 @@
+// base object for storing player's hp and enemies which are still alive on map
 var base = {
 	hp : 100,
 	alive_enemies : 5,
 	//interval for main game loop
 	interval : null,
 	
-	//functions
-	
+//functions
 	//called when an enemy is attacking the player's base
 	decreaseHP : function(damage){decHP(damage);},
 	lose : function(){defeated();},
@@ -14,8 +14,9 @@ var base = {
 	decreaseEnemies : function(){decEnemies();},
 	win : function(){defeat();}
 }
-	
-var decHP = function(d)
+
+//functions to be used for base object
+var decHP = function(d) // decreaseHP
 {
 	base.hp -= d;
 	console.log(base.hp);
@@ -23,12 +24,14 @@ var decHP = function(d)
 	if(base.hp <= 0)
 		base.lose();
 }
-var defeated = function()
+var defeated = function() // lose
 {
+	//Add turing on result screen when UI is ready
 	console.log("Lose");
+	//and then stops main game loop
 	clearInterval(base.interval);
 }
-var decEnemies = function()
+var decEnemies = function() // decreaseEnemies
 {
 	base.alive_enemies--;
 	if(base.alive_enemies <= 0)
@@ -36,8 +39,10 @@ var decEnemies = function()
 		base.win();
 	}
 }
-var defeat = function()
+var defeat = function() // win
 {
+	//Add turing on result screen when UI is ready
 	console.log("Win");
+	//and then stops main game loop
 	clearInterval(base.interval);
 }
