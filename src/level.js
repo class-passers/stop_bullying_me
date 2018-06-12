10fb4ca51fd122ef398a55b2a0b512eace04db01
Younggi Kim <cozyncoze@gmail.com>
2018-06-12 12:29:41 -0400

    resaved level0 map

--- src/level.js
+++ src/level.js
@@ -27,8 +27,39 @@ var levels = [
             }
         ],
         populate_timer : null,
-        map : level1_map,
+        map : level0_map,
 		start_money : 100
+    },
+    {   // for level 2
+        remaining_zombies : 15,
+        populate_zombie_info : [
+            {
+                start : 0,          // when to start populating a zombie in seconds
+                interval : 5,       // interval time for populating a zombie in seconds
+                type : "normal",    // what type of zombie
+                amount : 8,         // how many zombies are populated.
+                timer : null        // just for timer variable
+            },
+            {
+                start : 3,
+                interval : 4,
+                type : "fast",
+                amount : 6,
+                timer : null
+            }
+        ],
+        populate_boss_info : [
+            {
+                start : 30,
+                interval : 0,
+                type : "heavy",
+                amount : 1,
+                timer : null
+            }
+        ],
+        populate_timer : null,
+        map : level1_map,
+        start_money : 100
     },
     {   // for level 2
         remaining_zombies : 15,
@@ -60,5 +91,5 @@ var levels = [
         populate_timer : null,
         map : level2_map,
         start_money : 100
-    }
+    },
 ];
