6b016df74bb2888e57553843525cdd74e5fa5e23
Younggi Kim <cozyncoze@gmail.com>
2018-06-19 10:24:31 -0400

    fixed a bug that tried to draw image before it's loaded

--- src/UI_indicator.js
+++ src/UI_indicator.js
@@ -26,44 +26,46 @@ var IndicatorOjbect = function( indicatorType, pos_x, pos_y, txt_value)
 	this.get_x = function()
 	{
 		return Math.floor(this.x);
-	}
+	};
 	this.get_y = function()
 	{
 		return this.y;
-	}
+	};
 	this.get_source_x = function()
 	{
 		return this.curImage.sprite_width * (Math.floor(this.spriteIndex)%this.curImage.num_sprites_horz);
-	}
+	};
 	this.get_source_y = function()
 	{
 		return this.curImage.sprite_height * Math.floor(this.spriteIndex / this.curImage.num_sprites_horz);
-	}
+	};
 	this.get_sprite_width = function()
     {
         return this.curImage.sprite_width;
-    }
+    };
     this.get_sprite_height = function()
     {
         return this.curImage.sprite_height;
-    }
+    };
 	this.update = function(deltaTime)
 	{
 		this.spriteIndex += 10*deltaTime;
 		this.spriteIndex %= this.curImage.max_num_sprites
-	}
+	};
 	this.render = function(context)
 	{
-		//*
-		context.drawImage( this.curImage.image, this.get_source_x(), this.get_source_y(),
-            this.get_sprite_width(), this.get_sprite_height(),
-            this.get_x(), this.get_y(), this.width, this.height );
-		//*/
-			
-		context.fillStyle = this.txt_color;
-		context.font = this.txt_font;
-		context.textAlign = 'left';
-		context.textBaseline = 'top';
-		context.fillText(self.uiInfo.txt_sign + this.txt, this.txt_x, this.txt_y);
-	}
+		if( this.curImage.image ) {
+            //*
+            context.drawImage(this.curImage.image, this.get_source_x(), this.get_source_y(),
+                this.get_sprite_width(), this.get_sprite_height(),
+                this.get_x(), this.get_y(), this.width, this.height);
+            //*/
+
+            context.fillStyle = this.txt_color;
+            context.font = this.txt_font;
+            context.textAlign = 'left';
+            context.textBaseline = 'top';
+            context.fillText(self.uiInfo.txt_sign + this.txt, this.txt_x, this.txt_y);
+        }
+	};
 }
