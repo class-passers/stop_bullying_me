111388367af6a14254f07bb5e067986b15c14c3c
Younggi Kim <cozyncoze@gmail.com>
2018-08-08 13:45:00 -0400

    removed duplicated resource loading for the performance( separated an unit state to unit sprite )

--- src/human_images.js
+++ src/human_images.js
@@ -1,4 +1,4 @@
-var allHumanImages = {
+var allHumanImageSources = {
     normal : {
         idle: {
             image_src: "img/knight_idle.png",
@@ -12,26 +12,6 @@ var allHumanImages = {
         },
         walk: {
             image_src: "img/knight_walk.png",
-            max_num_sprites: 10,
-            num_sprites_horz: 5,
-            num_sprites_vert: 2,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveInTower: {
-            image_src: "img/knight_walk.png",
-            max_num_sprites: 10,
-            num_sprites_horz: 5,
-            num_sprites_vert: 2,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveOutTower: {
-            image_src: "img/knight_idle.png",
             max_num_sprites: 10,
             num_sprites_horz: 5,
             num_sprites_vert: 2,
@@ -73,16 +53,6 @@ var allHumanImages = {
             image: null
         },
         walk: {
-            image_src: "img/female_human_run.png",
-            max_num_sprites: 8,
-            num_sprites_horz: 3,
-            num_sprites_vert: 3,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveInTower: {
             image_src: "img/female_human_run.png",
             max_num_sprites: 8,
             num_sprites_horz: 3,
@@ -134,26 +104,6 @@ var allHumanImages = {
             repeat: true,
             image: null
         },
-        moveInTower: {
-            image_src: "img/ninja_run.png",
-            max_num_sprites: 10,
-            num_sprites_horz: 5,
-            num_sprites_vert: 2,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveOutTower: {
-            image_src: "img/ninja_idle.png",
-            max_num_sprites: 10,
-            num_sprites_horz: 5,
-            num_sprites_vert: 2,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
         dying: {
             image_src: "img/ninja_die.png",
             max_num_sprites: 10,
@@ -187,17 +137,6 @@ var allHumanImages = {
             image: null
         },
         walk: {
-            image_src: "img/heavy_knight_walk.png",
-            max_num_sprites: 7,
-            num_sprites_horz: 3,
-            num_sprites_vert: 3,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-
-        },
-        moveInTower: {
             image_src: "img/heavy_knight_walk.png",
             max_num_sprites: 7,
             num_sprites_horz: 3,
@@ -251,26 +190,6 @@ var allHumanImages = {
             repeat: true,
             image: null
         },
-        moveInTower: {
-            image_src: "img/wizard_walk.png",
-            max_num_sprites: 5,
-            num_sprites_horz: 2,
-            num_sprites_vert: 3,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveOutTower: {
-            image_src: "img/wizard_idle.png",
-            max_num_sprites: 5,
-            num_sprites_horz: 5,
-            num_sprites_vert: 1,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
         dying: {
             image_src: "img/wizard_die.png",
             max_num_sprites: 5,
@@ -313,16 +232,6 @@ var allHumanImages = {
             repeat: true,
             image: null
         },
-        moveInTower: {
-            image_src: "img/wizard_walk.png",
-            max_num_sprites: 5,
-            num_sprites_horz: 2,
-            num_sprites_vert: 3,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
         dying: {
             image_src: "img/wizard_die.png",
             max_num_sprites: 5,
@@ -346,17 +255,69 @@ var allHumanImages = {
     }
 };
 
+var allHumanImages = {
+    normal : {
+        idle: allHumanImageSources["normal"]["idle"],
+        walk: allHumanImageSources["normal"]["walk"],
+        moveInTower: allHumanImageSources["normal"]["walk"],
+        moveOutTower: allHumanImageSources["normal"]["walk"],
+        dying: allHumanImageSources["normal"]["dying"],
+        attack: allHumanImageSources["normal"]["attack"]
+    },
+    fast : {
+        idle: allHumanImageSources["fast"]["idle"],
+        walk: allHumanImageSources["fast"]["walk"],
+        moveInTower: allHumanImageSources["fast"]["walk"],
+        moveOutTower: allHumanImageSources["fast"]["walk"],
+        dying: allHumanImageSources["fast"]["dying"],
+        attack: allHumanImageSources["fast"]["attack"]
+    },
+    ranged : {
+        idle: allHumanImageSources["ranged"]["idle"],
+        walk: allHumanImageSources["ranged"]["walk"],
+        moveInTower: allHumanImageSources["ranged"]["walk"],
+        moveOutTower: allHumanImageSources["ranged"]["walk"],
+        dying: allHumanImageSources["ranged"]["dying"],
+        attack: allHumanImageSources["ranged"]["attack"]
+    },
+    heavy : {
+        idle: allHumanImageSources["heavy"]["idle"],
+        walk: allHumanImageSources["heavy"]["walk"],
+        moveInTower: allHumanImageSources["heavy"]["walk"],
+        moveOutTower: allHumanImageSources["heavy"]["walk"],
+        dying: allHumanImageSources["heavy"]["dying"],
+        attack: allHumanImageSources["heavy"]["attack"]
+    },
+    wizard : {
+        idle: allHumanImageSources["wizard"]["idle"],
+        walk: allHumanImageSources["wizard"]["walk"],
+        moveInTower: allHumanImageSources["wizard"]["walk"],
+        moveOutTower: allHumanImageSources["wizard"]["walk"],
+        dying: allHumanImageSources["wizard"]["dying"],
+        attack: allHumanImageSources["wizard"]["attack"]
 
-for( var type in allHumanImages ) {
-    if( allHumanImages.hasOwnProperty(type) ) {
-        for (var status in allHumanImages[type]) {
-            if( allHumanImages[type].hasOwnProperty( status ) ) {
-                allHumanImages[type][status].image = new Image();
-                allHumanImages[type][status].image.src = allHumanImages[type][status].image_src;
-                allHumanImages[type][status].image.onload = (function (type, it) {
+    },
+    healer : {
+        idle: allHumanImageSources["healer"]["idle"],
+        walk: allHumanImageSources["healer"]["walk"],
+        moveInTower: allHumanImageSources["healer"]["walk"],
+        moveOutTower: allHumanImageSources["healer"]["walk"],
+        dying: allHumanImageSources["healer"]["dying"],
+        attack: allHumanImageSources["healer"]["attack"]
+    }
+};
+
+
+for( var type in allHumanImageSources ) {
+    if( allHumanImageSources.hasOwnProperty(type) ) {
+        for (var status in allHumanImageSources[type]) {
+            if( allHumanImageSources[type].hasOwnProperty( status ) ) {
+                allHumanImageSources[type][status].image = new Image();
+                allHumanImageSources[type][status].image.src = allHumanImages[type][status].image_src;
+                allHumanImageSources[type][status].image.onload = (function (type, it) {
                     return function () {
-                        allHumanImages[type][it].sprite_width = Math.floor(allHumanImages[type][it].image.width / allHumanImages[type][it].num_sprites_horz);
-                        allHumanImages[type][it].sprite_height = Math.floor(allHumanImages[type][it].image.height / allHumanImages[type][it].num_sprites_vert);
+                        allHumanImageSources[type][it].sprite_width = Math.floor(allHumanImageSources[type][it].image.width / allHumanImageSources[type][it].num_sprites_horz);
+                        allHumanImageSources[type][it].sprite_height = Math.floor(allHumanImageSources[type][it].image.height / allHumanImageSources[type][it].num_sprites_vert);
                         numLoadedAssets++;
                     }
                 }(type, status));
--- src/zombie_images.js
+++ src/zombie_images.js
@@ -1,4 +1,4 @@
-var allZombieImages = {
+var allZombieImageSources = {
     normal : {
         idle: {
             image_src: "img/zombie_idle.png",
@@ -15,26 +15,6 @@ var allZombieImages = {
             max_num_sprites: 10,
             num_sprites_horz: 4,
             num_sprites_vert: 3,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveInTower: {
-            image_src: "img/zombie_walk.png",
-            max_num_sprites: 10,
-            num_sprites_horz: 4,
-            num_sprites_vert: 3,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveOutTower: {
-            image_src: "img/zombie_idle.png",
-            max_num_sprites: 15,
-            num_sprites_horz: 4,
-            num_sprites_vert: 4,
             sprite_width: 0,
             sprite_height: 0,
             repeat: true,
@@ -82,16 +62,6 @@ var allZombieImages = {
             repeat: true,
             image: null
         },
-        moveInTower: {
-            image_src: "img/female_zombie_walk.png",
-            max_num_sprites: 10,
-            num_sprites_horz: 5,
-            num_sprites_vert: 2,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
         dying: {
             image_src: "img/female_zombie_dying.png",
             max_num_sprites: 12,
@@ -125,17 +95,6 @@ var allZombieImages = {
             image: null
         },
         walk: {
-            image_src: "img/troll1_walk.png",
-            max_num_sprites: 7,
-            num_sprites_horz: 1,
-            num_sprites_vert: 7,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-
-        },
-        moveInTower: {
             image_src: "img/troll1_walk.png",
             max_num_sprites: 7,
             num_sprites_horz: 1,
@@ -190,26 +149,6 @@ var allZombieImages = {
             repeat: true,
             image: null
         },
-        moveInTower: {
-            image_src: "img/ranged_orc_walk.png",
-            max_num_sprites: 7,
-            num_sprites_horz: 4,
-            num_sprites_vert: 2,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveOutTower: {
-            image_src: "img/ranged_orc_idle.png",
-            max_num_sprites: 7,
-            num_sprites_horz: 2,
-            num_sprites_vert: 4,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
         dying: {
             image_src: "img/ranged_orc_dying.png",
             max_num_sprites: 7,
@@ -244,16 +183,6 @@ var allZombieImages = {
             image: null
         },
         walk: {
-            image_src: "img/orc1_walk.png",
-            max_num_sprites: 7,
-            num_sprites_horz: 2,
-            num_sprites_vert: 4,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveInTower: {
             image_src: "img/orc1_walk.png",
             max_num_sprites: 7,
             num_sprites_horz: 2,
@@ -305,26 +234,6 @@ var allZombieImages = {
             repeat: true,
             image: null
         },
-        moveInTower: {
-            image_src: "img/orc1_walk.png",
-            max_num_sprites: 7,
-            num_sprites_horz: 2,
-            num_sprites_vert: 4,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
-        moveOutTower: {
-            image_src: "img/orc1_idle.png",
-            max_num_sprites: 7,
-            num_sprites_horz: 1,
-            num_sprites_vert: 7,
-            sprite_width: 0,
-            sprite_height: 0,
-            repeat: true,
-            image: null
-        },
         dying: {
             image_src: "img/orc1_dying.png",
             max_num_sprites: 7,
@@ -348,24 +257,71 @@ var allZombieImages = {
     }
 };
 
+var allZombieImages = {
+    normal : {
+        idle: allZombieImageSources["normal"]["idle"],
+        walk: allZombieImageSources["normal"]["walk"],
+        moveInTower: allZombieImageSources["normal"]["walk"],
+        moveOutTower: allZombieImageSources["normal"]["walk"],
+        dying: allZombieImageSources["normal"]["dying"],
+        attack: allZombieImageSources["normal"]["attack"],
+    },
+    fast : {
+        idle: allZombieImageSources["fast"]["idle"],
+        walk: allZombieImageSources["fast"]["walk"],
+        moveInTower: allZombieImageSources["fast"]["walk"],
+        moveOutTower: allZombieImageSources["fast"]["walk"],
+        dying: allZombieImageSources["fast"]["dying"],
+        attack: allZombieImageSources["fast"]["attack"],
+    },
+    heavy : {
+        idle: allZombieImageSources["heavy"]["idle"],
+        walk: allZombieImageSources["heavy"]["walk"],
+        moveInTower: allZombieImageSources["heavy"]["walk"],
+        moveOutTower: allZombieImageSources["heavy"]["walk"],
+        dying: allZombieImageSources["heavy"]["dying"],
+        attack: allZombieImageSources["heavy"]["attack"],
+    },
+    ranged : {
+        idle: allZombieImageSources["ranged"]["idle"],
+        walk: allZombieImageSources["ranged"]["walk"],
+        moveInTower: allZombieImageSources["ranged"]["walk"],
+        moveOutTower: allZombieImageSources["ranged"]["walk"],
+        dying: allZombieImageSources["ranged"]["dying"],
+        attack: allZombieImageSources["ranged"]["attack"],
+    },
+    healer : {
+        idle: allZombieImageSources["healer"]["idle"],
+        walk: allZombieImageSources["healer"]["walk"],
+        moveInTower: allZombieImageSources["healer"]["walk"],
+        moveOutTower: allZombieImageSources["healer"]["walk"],
+        dying: allZombieImageSources["healer"]["dying"],
+        attack: allZombieImageSources["healer"]["attack"],
+    },
+    wizard : {
+        idle: allZombieImageSources["wizard"]["idle"],
+        walk: allZombieImageSources["wizard"]["walk"],
+        moveInTower: allZombieImageSources["wizard"]["walk"],
+        moveOutTower: allZombieImageSources["wizard"]["walk"],
+        dying: allZombieImageSources["wizard"]["dying"],
+        attack: allZombieImageSources["wizard"]["attack"],
+    }
+};
 
-for( var type in allZombieImages ) {
-    if( allZombieImages.hasOwnProperty(type) ) {
-        for (var status in allZombieImages[type]) {
-            if( allZombieImages[type].hasOwnProperty( status ) ) {
-                //console.log("load zombie of " + status + " in " + type );
-                allZombieImages[type][status].image = new Image();
-                allZombieImages[type][status].image.src = allZombieImages[type][status].image_src;
-                allZombieImages[type][status].image.onload = (function (type, it) {
-                    return function () {
-                        allZombieImages[type][it].sprite_width = Math.floor(allZombieImages[type][it].image.width / allZombieImages[type][it].num_sprites_horz);
-                        allZombieImages[type][it].sprite_height = Math.floor(allZombieImages[type][it].image.height / allZombieImages[type][it].num_sprites_vert);
-                        numLoadedAssets++;
-                    }
-                }(type, status));
-                numAllAssets++;
+
+for( var type in allZombieImageSources ) {
+    for (var status in allZombieImageSources[type]) {
+        //console.log("load zombie of " + status + " in " + type );
+        allZombieImageSources[type][status].image = new Image();
+        allZombieImageSources[type][status].image.src = allZombieImageSources[type][status].image_src;
+        allZombieImageSources[type][status].image.onload = (function (type, it) {
+            return function () {
+                allZombieImageSources[type][it].sprite_width = Math.floor(allZombieImageSources[type][it].image.width / allZombieImageSources[type][it].num_sprites_horz);
+                allZombieImageSources[type][it].sprite_height = Math.floor(allZombieImageSources[type][it].image.height / allZombieImageSources[type][it].num_sprites_vert);
+                numLoadedAssets++;
             }
-        }
+        }(type, status));
+        numAllAssets++;
     }
 }
 
